562e43102eef3b0645545481d286f1b177e097c9
MHMD_Alaa <mhmdalaa333@gmail.com>
2019-04-21 18:36:43 +0200

    finished op table

--- SIC-XE Assembler/src/SIC-XE Assembler.cpp
+++ SIC-XE Assembler/src/SIC-XE Assembler.cpp
@@ -9,39 +9,24 @@ using namespace std;
 struct struct_opcode {
 	string opcode;
 	int format;
-	struct_opcode() {
-		opcode = "undefined";
-		format = 0;
-	}
 };
-std::map<std::string, struct_opcode> opTable;
+map<string, struct_opcode> opTable;
 
-void loadOpTable() {
+void loadOpTable(string path) {
 	string line;
 	ifstream infile;
-	infile.open("optable.txt");
+	infile.open(path);
 	while (getline(infile, line)) {
-		std::regex r("(\\w+)\\s+(\\w+)\\s+(\\w+)");
-		std::smatch m;
-		std::regex_search(line, m, r);
-		std::istringstream iss (m[2]);
-		int x;
-		iss >> x;
-		opTable[m[1]].format = x;
+		regex r("(\\w+)\\s+(\\w+)\\s+(\\w+)");
+		smatch m;
+		regex_search(line, m, r);
+		opTable[m[1]].format = stoi(m[2]);
 		opTable[m[1]].opcode = m[3];
+		//cout << m[1] << "      " << opTable[m[1]].format<< "      " << opTable[m[1]].opcode << endl ;
 	}
 	infile.close();
 }
 int main() {
-//	std::string str("ADD         3      18");
-//	std::regex r("(\\w+)\\s+(\\w+)\\s+(\\w+)"); // entire match will be 2 numbers
-//	std::smatch m;
-//	std::regex_search(str, m, r);
-//
-//	for (unsigned int j = 1; j < m.size(); j++) {
-//		std::cout << m[j] << std::endl;
-//	}
-
-	loadOpTable();
+	loadOpTable("optable.txt");
 	return 0;
 }
