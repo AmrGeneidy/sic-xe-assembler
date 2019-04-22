1d400b2e6f5f2f0a01763dace6428be7b7002d3d
MHMD_Alaa <mhmdalaa333@gmail.com>
2019-04-23 01:02:08 +0200

    added header file for reference & add pass1Algorithm

--- SIC-XE Assembler/src/SIC-XE Assembler.cpp
+++ SIC-XE Assembler/src/SIC-XE Assembler.cpp
@@ -3,14 +3,22 @@
 #include<string>
 #include<fstream>
 #include<regex>
+#include<vector>
+
+#include"assemblerdata.h"
 
 using namespace std;
 
-struct struct_opcode {
-	string opcode;
-	int format;
-};
 map<string, struct_opcode> opTable;
+map<unsigned int, listing_line> listing_table;
+map<string, unsigned int> symbol_table;
+
+unsigned int starting_address;
+unsigned int program_length;
+
+unsigned int current_line_number;
+unsigned int LOCCTR;
+listing_line current_line;
 
 void loadOpTable(string path) {
 	string line;
@@ -22,9 +30,71 @@ void loadOpTable(string path) {
 		regex_search(line, m, r);
 		opTable[m[1]].format = stoi(m[2]);
 		opTable[m[1]].opcode = m[3];
+		//TODO delete this line
 		//cout << m[1] << "      " << opTable[m[1]].format<< "      " << opTable[m[1]].opcode << endl ;
 	}
 	infile.close();
+}
+
+void pass1_Algorithm(string codePath) {
+	//TODO load listing_table
+	loadOpTable("optable.txt");
+	current_line_number = 1;
+
+	//skip the comments
+	while (listing_table[current_line_number].isAllComment) {
+		current_line_number++;
+	}
+
+	current_line = listing_table[current_line_number];
+	//check start addressing
+	if (iequals(current_line.mnemonic, "START")) {
+		starting_address = stoi(current_line.operand, 0, 16);
+		listing_table[current_line_number].address = starting_address;
+		current_line = listing_table[++current_line_number];
+	} else {
+		starting_address = 0;
+	}
+	LOCCTR = starting_address;
+
+	//reading code loop
+	while (!iequals(current_line.mnemonic, "END")) {
+		//process the line if not a comment
+		if (!current_line.isAllComment) {
+			//assign address to the line
+			listing_table[current_line_number].address = LOCCTR;
+
+			//process the label field
+			if (!current_line.label.empty()) {
+				if (symbol_table.find(current_line.label)
+						!= symbol_table.end()) {
+					listing_table[current_line_number].error.push_back("symbol '"
+							+ current_line.label + "' is already defined") ;
+				} else {
+					symbol_table[current_line.label] = LOCCTR;
+				}
+			}
+
+			//process the mnemonic
+			if(opTable.find(current_line.mnemonic) != opTable.end()){
+				//not directive
+				if (opTable[current_line.mnemonic].format == 3 && current_line.isFormat4) {
+					LOCCTR += 4;
+				}else if (opTable[current_line.mnemonic].format == 2 && current_line.isFormat4) {
+					listing_table[current_line_number].error.push_back("Can't use format 4 with mnemonic " + current_line.mnemonic) ;
+				}else {
+					LOCCTR += opTable[current_line.mnemonic].format;
+				}
+			}else if(false /*TODO handle all the directives*/){
+
+			}else{
+				listing_table[current_line_number].error.push_back("Invalid operation code") ;
+			}
+		}//end line process
+		current_line = listing_table[++current_line_number];
+	}
+	program_length = LOCCTR - starting_address;
+
 }
 int main() {
 	loadOpTable("optable.txt");
---
+++ SIC-XE Assembler/src/assemblerdata.h
@@ -0,0 +1,51 @@
+#include<map>
+#include<string>
+
+using namespace std;
+
+#ifndef ASSEMBLERDATA_H_
+#define ASSEMBLERDATA_H_
+
+
+struct struct_opcode {
+	string opcode;
+	unsigned int format;
+};
+
+struct listing_line {
+	bool isAllComment;
+	unsigned int address;
+	bool isFormat4;
+	string label;
+	string mnemonic;
+	string operand;
+	string comment;
+    vector<string> error;
+};
+
+
+extern map<string, struct_opcode> opTable;
+extern map<unsigned int, listing_line> listing_table;
+//key: label name ,  value: address
+extern map<string, unsigned int> symbol_table;
+
+extern unsigned int starting_address;
+extern unsigned int program_length;
+
+
+
+//helping functions
+
+//iequals function check equality of two strings CASE-INSENSITIVE
+struct iequal {
+	bool operator()(int c1, int c2) const {
+		return std::toupper(c1) == std::toupper(c2);
+	}
+};
+
+bool iequals(const std::string& str1, const std::string& str2) {
+	return std::equal(str1.begin(), str1.end(), str2.begin(), iequal());
+}
+//end iequals
+
+#endif /* ASSEMBLERDATA_H_ */
